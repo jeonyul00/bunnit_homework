84c10b412f3806f8f0c52679f99269ef275e0103
jeonyul <jeonyul00@gmail.com>
2025-07-14 19:53:33 +0900

    create app && set: lint && prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  printWidth: 150,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+};
